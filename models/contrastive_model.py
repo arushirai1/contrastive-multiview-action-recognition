f6d7e86c32909e5e43a5780d9039b9cee7f31a53
Arushi Rai <arushirai1999@gmail.com>
2021-02-02 04:14:01 -0600

    Fix sequential modules

--- models/contrastive_model.py
+++ models/contrastive_model.py
@@ -12,7 +12,7 @@ class ContrastiveModel(nn.Module):
 
     def forward(self, x):
         # change shape so computation is done in parallel
-        x = x.view(-1, x.shape[1]*x.shape[2], 3, 8, 112, 112)
+        x = x.view(-1, x.shape[1]*x.shape[2], x.shape[3], x.shape[4], x.shape[5], x.shape[6])
         return self.base_model(x)
 
     def get_repr(self, x):
